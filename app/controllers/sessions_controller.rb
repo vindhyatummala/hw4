7629ae67d466f6357c2dbe443f44a44206d4d781
vindhyatummala <vindhya.tummala@kellogg.northwestern.edu>
2023-03-05 22:37:13 +0000

    Completing the assignment

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,26 @@
 class SessionsController < ApplicationController
+  
   def new
   end
 
-  def create
-  end
+    def create
+        @user = User.find_by({"email" => params["email"]})
+        if @user
+            if params["password"] == @user["password"]
+                redirect_to "/places"
+            else 
+                redirect_to "/sessions/new"
+            end
+        else
+            redirect_to "/sessions/new"
+        end
+    end
 
   def destroy
+    flash["notice"] = "Goodbye."
+    session["user_id"] = nil
+    redirect_to "/login"
   end
+
 end
   
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,10 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new
-    @user["first_name"] = params["user"]["first_name"]
-    @user["last_name"] = params["user"]["last_name"]
+    @user["username"] = params["user"]["username"]
     @user["email"] = params["user"]["email"]
     @user["password"] = params["user"]["password"]
     @user.save
-    redirect_to "/"
+    redirect_to "/places"
   end
 end
