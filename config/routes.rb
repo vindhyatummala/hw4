d90a56a59357c3b3b2b95d6b34585966a6331912
vindhyatummala <vindhya.tummala@kellogg.northwestern.edu>
2023-03-08 00:16:06 +0000

    Completing the assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get "/", :controller => "places", :action => "index"
+  get "/login", :controller => "sessions", :action => "new"
+  get "/logout", :controller => "sessions", :action => "destroy"
   resources "places"
   resources "posts"
   resources "sessions"
